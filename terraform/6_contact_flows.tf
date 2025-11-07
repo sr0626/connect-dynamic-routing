ce0cb3f59affd596318b54854c8f1e2a65263713
Sateesh Rudrangi <sateesh.rudrangi@ptpinc.com>
2025-11-06 19:16:33 -0600

    template file for contact flow

--- terraform/6_contact_flows.tf
+++ terraform/6_contact_flows.tf
@@ -4,9 +4,16 @@ resource "aws_connect_contact_flow" "test" {
   description  = "My Dynamic Routing Flow"
   type         = "CONTACT_FLOW"
   content      = file("${path.module}/../json/contact_flows/MyDynamicRoutingFlow.json")
+  #content      = local.contact_flow_content
   content_hash = filebase64sha256("${path.module}/../json/contact_flows/MyDynamicRoutingFlow.json")
 }
 
+locals {
+  contact_flow_content = templatefile("${path.module}/../json/contact_flows/MyDynamicRoutingFlow.json", {
+    queue_arn = aws_connect_queue.test.arn
+    queue_id  = aws_connect_queue.test.id
+  })
+}
 
 data "aws_connect_contact_flow" "basic" {
   instance_id = aws_connect_instance.test.id
